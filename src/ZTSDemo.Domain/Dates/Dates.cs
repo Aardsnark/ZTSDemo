15bdf9dde8ea71ca693d3117471e6f23c77f9177
Aardsnark <Aardsnark>
2023-10-17 10:27:16 +0200

    Dates AgR

---
+++ src/ZTSDemo.Domain/Dates/Dates.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+
+namespace ZTSDemo.Dates
+{
+    public class Date : AggregateRoot<Guid>
+    {
+        public virtual DateTime? Datetime { get; protected set; }
+        public virtual int Year { get; protected set; }
+        public virtual int Month { get; protected set; }
+        public virtual int Day { get; protected set;}
+        public virtual int Hour { get; protected set; }
+
+        protected Date()
+        {
+
+        }
+
+        public Date(Guid id)
+            : base(id)
+        {
+
+        }
+    }
+}
